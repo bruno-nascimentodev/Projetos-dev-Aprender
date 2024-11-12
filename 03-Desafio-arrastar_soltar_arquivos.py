4bd548c452daaf1b2e8faf88bbb0171cae200655
bruno-nascimentodev <nascimento.webmaster@gmail.com>
2024-11-12 20:13:28 -0300

    # DESAFIO - Arrastas arquivos de uma pasta para outra

---
+++ 03-Desafio-arrastar_soltar_arquivos.py
@@ -0,0 +1,8 @@
+# DESAFIO - Arrastas arquivos de uma pasta para outra
+
+import pyautogui as py
+
+# LooP para arrastar 9 arquivos de uma pasta para outra
+for i in range(9):
+    py.moveTo(1276,231,duration=0.5)
+    py.dragTo(1289,776,duration=0.5)
